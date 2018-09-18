7113b6db6ef09fde5864590ddd525ef7ea98342f
Michelle Chen <michchen58@gmail.com>
2018-09-17 20:26:05 -0700

    Test on CircleCI

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,11 +1,6 @@
-const functions = require('./functions');
 const { addCommas, renderPrice } = require('../client/src/components/ProductInfo.js');
 
 // addCommas
-
-test('Adds 2 + 2 to equal 4', () => {
-  expect(functions.sum(2, 2)).toBe(4);
-});
 
 test('Adds comma to after the first digit in 1000', () => {
   expect(addCommas(1000)).toBe('1,000');
