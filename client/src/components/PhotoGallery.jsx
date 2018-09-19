b4e606f32a9e704b4d33364621bff21cf0f73ee6
Michelle Chen <michchen58@gmail.com>
2018-09-18 20:47:36 -0700

    Adjust CSS to integrate PhotoGallery into ProductInfo

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -9,11 +9,12 @@ class PhotoGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPhoto: 'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/1958562_232816943589414_1989928331_n.jpg?_nc_cat=0&oh=4b14fc7bca42f9aa7f451722d2aebe1e&oe=5C27D523',
+      currentPhoto: 'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/20800148_884991911705244_8847631967476084829_n.jpg?_nc_cat=0&oh=f65b967c668c091ea0b530193175533d&oe=5C1E5825',
       photoList: [
+        'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/20800148_884991911705244_8847631967476084829_n.jpg?_nc_cat=0&oh=f65b967c668c091ea0b530193175533d&oe=5C1E5825',
         'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/1958562_232816943589414_1989928331_n.jpg?_nc_cat=0&oh=4b14fc7bca42f9aa7f451722d2aebe1e&oe=5C27D523',
-        'https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg',
-        'https://r.hswstatic.com/w_907/gif/tesla-cat.jpg'],
+        'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10306256_330658910471883_1485038244507827308_n.jpg?_nc_cat=0&oh=70e12fd4f4839e4be287cf09bcfba61c&oe=5C2D6494',
+      ]
     };
     this.handleClick = this.handleClick.bind(this);
   }
